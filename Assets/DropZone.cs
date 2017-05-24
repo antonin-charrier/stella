45ec8a13a99b628ec6c7fe5f902d25169d81ce5d
Antonin Home <acharrier@intechinfo.fr>
2017-05-25 00:06:25 +0200

    Added OPPONENT_TABLETOP that is not Droppable

--- Assets/DropZone.cs
+++ Assets/DropZone.cs
@@ -5,29 +5,39 @@ using UnityEngine.EventSystems;
 
 public class DropZone : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler {
 
-	public enum dropZones {HAND, TABLETOP, DECK}
+	public enum dropZones {HAND, TABLETOP, DECK, OPPONENT_TABLETOP}
 
 	public void OnPointerEnter(PointerEventData eventData) {
 		if (eventData.pointerDrag == null)
 			return;
 		
 		Draggable d = eventData.pointerDrag.GetComponent<Draggable> ();
-        Debug.Log(d.placeholderParent.name);
 		if (d != null) {
 			d.placeholderParent = this.transform;
-            if (d.placeholderParent.name == dropZones.TABLETOP.ToString()) {
+            if (d.placeholderParent.name == dropZones.TABLETOP.ToString())
+            {
                 d.isDroppable = true;
-            } else if (d.placeholderParent.name == dropZones.DECK.ToString()) {
+            }
+            else if (d.placeholderParent.name == dropZones.DECK.ToString())
+            {
                 d.isDroppable = false;
-            } else if (d.placeholderParent.name == dropZones.HAND.ToString()) {
+            }
+            else if (d.placeholderParent.name == dropZones.HAND.ToString())
+            {
                 if (d.parentToReturnTo.name == dropZones.HAND.ToString())
                 {
                     d.isDroppable = true;
-                } else
+                }
+                else
                 {
                     d.isDroppable = false;
                 }
-            } else {
+            }
+            else if (d.placeholderParent.name == dropZones.OPPONENT_TABLETOP.ToString())
+            {
+                d.isDroppable = false;
+            }
+            else {
                 d.isDroppable = false;
             }
 		}
@@ -35,10 +45,8 @@ public class DropZone : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPoin
 
 	public void OnDrop(PointerEventData eventData) {
 		Draggable d = eventData.pointerDrag.GetComponent<Draggable> ();
-		Debug.Log (d.parentToReturnTo.name);
 		if (d != null && d.isDroppable) {
 			d.parentToReturnTo = this.transform;
-			Debug.Log (d.parentToReturnTo.name);
 		}
 	}
 

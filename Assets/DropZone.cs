774d83ba770c6dace777a3de85eb4026ddb89b95
Antonin Home <acharrier@intechinfo.fr>
2017-05-24 21:41:04 +0200

    Cards draggables and droppable in Hand and Tabletop zones

---
+++ Assets/DropZone.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DropZone : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler {
+
+	public enum dropZones {HAND, TABLETOP, DECK}
+	public bool isDroppable = false;
+
+	public void OnPointerEnter(PointerEventData eventData) {
+		if (eventData.pointerDrag == null)
+			return;
+		
+		Draggable d = eventData.pointerDrag.GetComponent<Draggable> ();
+		if (d != null) {
+			d.placeholderParent = this.transform;
+			if (d.placeholderParent.name == dropZones.TABLETOP.ToString ()) {
+				isDroppable = true;
+			} else if (d.placeholderParent.name == dropZones.DECK.ToString ()) {
+				isDroppable = false;
+			} else if (d.placeholderParent.name == dropZones.HAND.ToString ()) {
+				isDroppable = false;
+			} else {
+				isDroppable = false;
+			}
+		}
+	}
+
+	public void OnDrop(PointerEventData eventData) {
+		Draggable d = eventData.pointerDrag.GetComponent<Draggable> ();
+		Debug.Log (d.parentToReturnTo.name);
+		if (d != null && isDroppable) {
+			d.parentToReturnTo = this.transform;
+			Debug.Log (d.parentToReturnTo.name);
+		}
+	}
+
+	public void OnPointerExit(PointerEventData eventData) {
+		if (eventData.pointerDrag == null)
+			return;
+		
+		Draggable d = eventData.pointerDrag.GetComponent<Draggable> ();
+		if (d != null && d.placeholderParent==this.transform) {
+			d.placeholderParent = d.parentToReturnTo;
+		}
+	}
+}

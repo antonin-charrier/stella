8a0a885282acae86a2ae1984ebda74940c0d17cd
Antonin Home <acharrier@intechinfo.fr>
2017-06-21 22:20:24 +0200

    Card Instantiation

---
+++ Assets/Resources/Canvas/Instantiation.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.UI;
+
+public class Instantiation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        InstantiateCard ("card name", 3, 2, 11, "description");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private GameObject InstantiateCard(string name, int level, int power, int lifePoints, string description)
+    {
+        Card card = new Card(name, level, power, lifePoints, description);
+
+        GameObject cardGameObject = (GameObject)Instantiate(Resources.Load("Card/Card"));
+        cardGameObject.transform.parent = GameObject.Find("HAND").transform;
+
+        Transform titleTransform = cardGameObject.transform.Find("CardTitleLabel");
+        Text nameText = titleTransform.GetComponent<Text>();
+        nameText.text = name;
+
+        Transform powerTransform = cardGameObject.transform.Find("CardPowerLabel");
+        Text powerText = powerTransform.GetComponent<Text>();
+        powerText.text = power.ToString();
+
+        Transform lifePointsTransform = cardGameObject.transform.Find("CardLifePointsLabel");
+        Text lifePointsText = lifePointsTransform.GetComponent<Text>();
+        lifePointsText.text = lifePoints.ToString();
+
+        Transform descriptionTransform = cardGameObject.transform.Find("CardDescriptionLabel");
+        Text descriptionText = descriptionTransform.GetComponent<Text>();
+        descriptionText.text = description;
+
+        DisplayLevelStars(cardGameObject, 7);
+
+        return cardGameObject;
+    }
+
+
+    private void DisplayLevelStars(GameObject cardGameObject)
+    {
+        GameObject levelSprites = cardGameObject.transform.Find("CardLevelSprites").gameObject;
+        GameObject levelSprite = (GameObject)Instantiate(Resources.Load("Card/CardLevelSprite"));
+        levelSprite.transform.SetParent(levelSprites.transform);
+    }
+
+    private void DisplayLevelStars(GameObject cardGameObject, int level)
+    {
+        GameObject levelSprites = cardGameObject.transform.Find("CardLevelSprites").gameObject;
+        for (int i = 0; i < level; i++)
+        {
+            GameObject levelSprite = (GameObject)Instantiate(Resources.Load("Card/CardLevelSprite"));
+            levelSprite.transform.SetParent(levelSprites.transform);
+        }
+    }
+}
---
+++ Assets/Resources/Card/Card.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+using System.Collections;
+using System.Xml;
+using System.Xml.Serialization;
+
+public class Card : MonoBehaviour
+{
+    [XmlAttribute("name")]
+    public string name;
+
+    [XmlElement("Level")]
+    public int level;
+
+    [XmlElement("Power")]
+    public int power;
+
+    [XmlElement("LifePoints")]
+    public int lifePoints;
+
+    [XmlElement("Description")]
+    public string description;
+
+
+    public Card(string name, int level, int power, int lifePoints, string description)
+    {
+        this.name = name;
+        this.level = level;
+        this.power = power;
+        this.lifePoints = lifePoints;
+        this.description = description;
+    }
+
+}

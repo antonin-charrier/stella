e098ab7ea7f406125d499385d986b36c8c59d78f
Antonin Home <acharrier@intechinfo.fr>
2017-06-22 22:24:58 +0200

    All databases Loaders factorized in one Loader.cs

--- Assets/Resources/Loader/Loader.cs
+++ Assets/Resources/Loader/Loader.cs
@@ -1,18 +1,32 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CardLoader : MonoBehaviour
+public class Loader : MonoBehaviour
 {
 
     public const string cardLoaderPath = "Loader/cards";
+    public const string deckLoaderPath = "Loader/decks";
+    public const string deckCardLoaderPath = "Loader/deck_cards";
 
-	void Start() 
+    void Start() 
     {
         CardContainer cc = CardContainer.Load(cardLoaderPath);
+        DeckContainer dc = DeckContainer.Load(deckLoaderPath);
+        DeckCardContainer dcc = DeckCardContainer.Load(deckCardLoaderPath);
 
         foreach (Card card in cc.cards)
         {
             InstantiateCard(card.name, card.level, card.power, card.lifePoints, card.description);
+        }
+
+        foreach (Deck deck in dc.decks)
+        {
+            Debug.Log(deck.name);
+        }
+
+        foreach (DeckCard deckCard in dcc.deckCards)
+        {
+            Debug.Log(deckCard.name);
         }
 	}
 
